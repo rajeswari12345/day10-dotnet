d4977da0ff652561de59a98c59c2649f8f09fb0e
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 17:10:19 +0530

    day10 uc6

---
+++ day10 uc6.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+static void Main(string[] args)
+{
+
+    Person1 person1   = new Person1();
+
+    Console.Write("Enter First Name: ");
+    person1.FirstName = Console.ReadLine();
+
+    Console.Write("Enter Last Name: ");
+    person1.LastName = Console.ReadLine();
+
+    Console.Write("Enter Phone Number: ");
+    person1. PhoneNumber = Console.ReadLine();
+
+    Console.Write("Enter address: ");
+    person1 .address = Console.ReadLine();
+
+    Console.Write("Enter city: ");
+    person1 .city = Console.ReadLine();
+
+    Console.Write("Enter state: ");
+    person1.state = Console.ReadLine();
+
+    Console.Write("Enter mail id: ");
+    person1.mail id = Console.ReadLine();
+
+    Console.Write(" Enter zip: ");
+    person1.zip = Console.ReadLine();
+}
+Person2  person2 = new Person2();
+Console.Write("Enter First Name: ");
+person2.FirstName = Console.ReadLine();
+
+Console.Write("Enter Last Name: ");
+person2 .LastName = Console.ReadLine();
+
+Console.Write("Enter Phone Number: ");
+person2 .PhoneNumber = Console.ReadLine();
+
+Console.Write("Enter address: ");
+person2 .address = Console.ReadLine();
+
+Console.Write("Enter city: ");
+person2.city = Console.ReadLine();
+
+Console.Write("Enter state: ");
+person2  .state = Console.ReadLine();
+
+Console.Write("Enter mail id: ");
+person2 .mail id = Console.ReadLine();
+
+Console.Write(" Enter zip: ");
+person2 .zip = Console.ReadLine();
+    }
+}
