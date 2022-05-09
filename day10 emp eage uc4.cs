cc16379cea6cfc743bad77784b0df19308f5ac15
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 17:28:51 +0530

    day10 empwage uc4

---
+++ day10 emp eage uc4.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class program
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    static void Main(String[] args)
+    {
+        //variables
+        int empHrs = 0;
+            int empwage = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
+
+        }
+
+        empwage= empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage :"  + empwage);
+    }
+}
