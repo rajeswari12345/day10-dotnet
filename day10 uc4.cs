12fd4f1c3e7c0247617619f74095a63c8be9b08f
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 17:09:23 +0530

    day10 uc4

---
+++ day10 uc4.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Are you sure you want to delete this person from your address book? (Y/N)");
+PrintPerson(person);
+
+if (Console.ReadKey().Key == ConsoleKey.Y)
+{
+    People.delete(person);
+    Console.WriteLine("Person deleted. Press any key to continue.");
+    Console.ReadKey();
+}
